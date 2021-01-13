f7a9688c0902a3435e951ab17e0ba11caa41cef4
solomonhaile <72422658+solomonhaile@users.noreply.github.com>
2021-01-12 21:14:07 -0800

    Netflix-Clone

---
+++ build/precache-manifest.e35193b14e9a9f04fbeb58a272571b7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567d7a632f88b1b92803440c71e1bbb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dddab61bfdf5379dd0db",
+    "url": "/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "92f2502db03248df14ab",
+    "url": "/static/js/2.7199c33c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.7199c33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dddab61bfdf5379dd0db",
+    "url": "/static/js/main.62cf59ca.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
